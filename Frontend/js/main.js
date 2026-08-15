12d686af6bb3a62d716e3b539e946437cdd31995
MartinNagypal <martinbeka8@hotmail.com>
2026-08-15 04:44:34 +0300

    added a console page for fullscreen console and design changes

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,9 +1,15 @@
 const dashboardButton = document.getElementById("dashboardButton");
+const consoleButton = document.getElementById("consoleButton");
 const backupsButton = document.getElementById("backupsButton");
 const settingsButton = document.getElementById("settingsButton");
+const consoleExpandButton = document.getElementById("consoleExpandButton");
 
 dashboardButton.addEventListener("click", () => {
     window.location.href = "index.html";
+});
+
+consoleButton.addEventListener("click", () => {
+    window.location.href = "console.html";
 });
 
 backupsButton.addEventListener("click", () => {
@@ -12,4 +18,12 @@ backupsButton.addEventListener("click", () => {
 
 settingsButton.addEventListener("click", () => {
     window.location.href = "settings.html";
+});
+
+consoleExpandButton.addEventListener("click", () => {
+    if (window.location.href.includes("console.html")) {
+        window.location.href = "index.html";
+    } else {
+        window.location.href = "console.html";
+    }
 });
